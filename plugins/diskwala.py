aa98c465dcaeee74a91ff175e7111178ed7dd7a9
netlify294-coder <netlify294@gmail.com>
2026-07-31 14:29:07 +0530

    Update diskwala.py

--- plugins/diskwala.py
+++ plugins/diskwala.py
@@ -1328,7 +1328,7 @@ async def store_video_for_link(app: Client, link: str, status_msg: Message, tag:
         if await get_cache(link):
             return
 
-        await status_msg.edit_text(f"<b>📨 𝖱𝖤𝖰𝖴𝖤𝖲𝖳𝖨𝖭𝖦 𝖵𝖨𝖠 @{DISKWALADSBOT}... {tag}</b>")
+        await status_msg.edit_text(f"<b>📨 𝖵𝖨𝖠 {}... {tag}</b>")
         vid_msg = await fetch_via_diskwaladsbot(link)
 
         media = vid_msg.video or vid_msg.document
